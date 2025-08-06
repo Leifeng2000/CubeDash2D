a622cd4d3a7d1d7bc518fc6d218092f96086253d
Leifeng2000 <leifeng7112@gmail.com>
2025-08-06 11:17:46 +0700

    Update

---
+++ Assets/CubeDash/Scripts/MenuSelect.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuSelect : MonoBehaviour {
+
+	public GameObject mainUI;
+	public GameObject gameplayUI;
+	public GameObject pauseUI;
+	public GameObject pauseButton;
+	public GameObject gameOverUI;
+	private AudioSource buttonClick;
+
+	private PlayerMovement playerMovement;
+	private SpawnObstacles spawnObstacles;
+
+	void Start() {
+		playerMovement = GameObject.Find ("square").GetComponent<PlayerMovement> ();
+		spawnObstacles = GameObject.Find ("Canvas").GetComponent<SpawnObstacles> ();
+
+		buttonClick = GameObject.Find ("buttonClick").GetComponent<AudioSource> ();
+		mainUI.transform.Find ("bestScore").GetComponent<Text> ().text = "best score: " + PlayerPrefs.GetInt ("bestScore", 0);
+	}
+
+	public void play() {
+		playerMovement.enabled = true;
+		spawnObstacles.enabled = true;
+		mainUI.SetActive (false);
+		gameplayUI.SetActive (true);
+		pauseButton.SetActive (true);
+		buttonClick.Play ();
+	}
+	public void pause() {
+		playerMovement.enabled = false;
+		Time.timeScale = 0;
+		pauseUI.SetActive (true);
+		pauseButton.SetActive (false);
+		buttonClick.Play ();
+	}
+	public void resume() {
+		Time.timeScale = 1;
+		pauseUI.SetActive (false);
+		pauseButton.SetActive (true);
+		playerMovement.enabled = true;
+		buttonClick.Play ();
+	}
+	public void gameOver() {
+		GameObject.Find ("gameOver").GetComponent<AudioSource> ().Play ();
+		if (playerMovement.score > PlayerPrefs.GetInt ("bestScore", 0)) {
+			PlayerPrefs.SetInt ("bestScore", playerMovement.score);
+		}
+		gameOverUI.SetActive (true);
+		pauseButton.SetActive (false);
+		Time.timeScale = 0;
+	}
+	public void restart() {
+		Time.timeScale = 1;
+		GameObject[] obstacles = GameObject.FindGameObjectsWithTag ("obstacle");
+		for (int i = 0; i < obstacles.Length; i++) {
+			Destroy (obstacles [i]);
+		}
+		GameObject.Find ("square").transform.position = new Vector2 (-3, 0.6f);
+		playerMovement.score = 0;
+		playerMovement.up = true;
+		playerMovement.positionX = -6;
+		playerMovement.positionY = 0.6f;
+		playerMovement.speedUp = 0.01f;
+		playerMovement.timer = 0;
+
+		GameObject.Find("score").GetComponent<Text> ().text = "score: 0";
+		GameObject.Find ("Canvas").GetComponent<SpawnObstacles> ().deletedObstacleLevel = 0;
+		GameObject.Find ("Canvas").GetComponent<SpawnObstacles> ().obstacleLevel = 1;
+		GameObject.Find ("Canvas").GetComponent<SpawnObstacles> ().lastObstaclePosition = 20.48f;
+
+		playerMovement.enabled = false;
+		spawnObstacles.enabled = false;
+		mainUI.SetActive (true);
+		gameplayUI.SetActive (false);
+		mainUI.transform.Find ("bestScore").GetComponent<Text> ().text = "best score: " + PlayerPrefs.GetInt ("bestScore", 0);
+		gameOverUI.SetActive (false);
+		buttonClick.Play ();
+	}
+}
---
+++ Assets/CubeDash/Scripts/PlayerMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public Rigidbody2D rb;
+	public GameObject scoreUI;
+	private Text scoreText;
+	public float positionX = -6;
+	public float positionY = 0.6f;
+	public bool up = true;
+	public float speedUp = 0.01f;
+	private AudioSource changeLinesSound;
+	public float timer = 0;
+	public int score = 0;
+
+	private int pauseButtonX;
+	private int pauseButtonY;
+	void Start() {
+		changeLinesSound = GameObject.Find ("changeLines").GetComponent<AudioSource> ();
+		scoreText = scoreUI.GetComponent<Text> ();
+		pauseButtonX = Screen.width / 10;
+		pauseButtonY = Screen.height - (int)(Screen.height - Screen.height / 1.2f);
+	}
+	// Update is called once per frame
+	void FixedUpdate () {
+		speedUp += 0.00001f;
+		positionX += (0.05f + speedUp);
+		rb.GetComponent<Rigidbody2D> ().position = new Vector2 (positionX, positionY);
+	}
+	void Update() {
+		timer += Time.deltaTime;
+		if (timer >= 1) {
+			timer = 0;
+			score += 10;
+			scoreText.text = "score: " + score;
+		}
+		if (Input.GetMouseButtonDown (0)) {
+			if ((Input.mousePosition.y > pauseButtonY && Input.mousePosition.x < pauseButtonX) == false) {//detect if player has not clicked on pause button on screen
+				changeLinesSound.Play ();
+				if (up) {
+					up = false;
+					positionY = -0.6f;
+				} else {
+					up = true;
+					positionY = 0.6f;
+				}
+			}
+		}
+
+
+	}
+	void OnTriggerEnter2D(Collider2D col) {
+		GameObject.Find ("Canvas").GetComponent<MenuSelect> ().gameOver ();
+	}
+}
